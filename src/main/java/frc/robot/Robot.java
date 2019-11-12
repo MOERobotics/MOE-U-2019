125ceb8b67c96c9086971b14b0885655eaae47d1
Kathryn Zhang <kathrynzhang@live.com>
2019-11-11 20:29:53 -0500

    Turn to North code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,6 +29,7 @@ public class Robot extends TimedRobot {
   Encoder rightEncoder = new Encoder(2,3,false, CounterBase.EncodingType.k1X);
 
   AHRS navx = new AHRS(SPI.Port.kMXP, (byte) 50);
+  boolean buttonNorth = false;
 
   public Robot() {
     motorRight1.setInverted(true);
@@ -54,6 +55,7 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("leftEncoderValue", leftEncoder.get());
     SmartDashboard.putNumber("rightEncoderValue", rightEncoder.get());
     SmartDashboard.putNumber("heading", navx.getYaw());
+    SmartDashboard.putBoolean("buttonNorth", buttonNorth);
   }
 
   @Override
@@ -79,11 +81,16 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+      rightEncoder.reset();
+      leftEncoder.reset();
   }
 
   @Override
   public void teleopPeriodic() {
 
+      if (leftJoystick.getRawButton(1)){
+          buttonNorth = false;
+      }
     //double speedPct = 100;
     //
     //speedPct = - leftJoystick.getY();
@@ -96,14 +103,80 @@ public class Robot extends TimedRobot {
     //motorRight3.set(ControlMode.PercentOutput, speedPct);
 
 
-    double leftSpeed = 0;
-    double rightSpeed = 0;
+      double leftSpeed = 0;
+      double rightSpeed = 0;
 
-    double joyY = - leftJoystick.getY();
+    double joyY = -leftJoystick.getY();
     double joyX = leftJoystick.getX();
 
-    leftSpeed = joyY + joyX;
-    rightSpeed = joyY - joyX;
+    //leftSpeed = joyY + joyX;
+    //rightSpeed = joyY - joyX;
+    //make sure to build every time you boot up build
+    int buttonHold = 1;
+
+    //if pressing button 1, robot rolls straight ahead slowly
+
+      /**
+      //find a way to reset navx when first pressed
+      boolean firstHold = false;
+
+      if (!leftJoystick.getRawButton(buttonHold)){
+          firstHold = true;
+      }
+       if (leftJoystick.getRawButton(buttonHold)) {
+          if (firstHold == true) {
+              navx.reset();
+          }
+          //firstHold = false;
+
+          double currentYaw = navx.getYaw();
+          leftSpeed = 0.25;
+          rightSpeed = 0.25;
+
+          if (currentYaw > 1) {
+              rightSpeed = 0.30;
+
+          } else if (currentYaw < -1) {
+              leftSpeed = 0.30;
+
+          }
+      }
+
+
+
+       if (leftJoystick.getRawButton(buttonHold)) {
+       leftSpeed = 0.25;
+       rightSpeed = 0.25;
+
+        if (leftEncoder.get() - rightEncoder.get() > 5) {
+            rightSpeed = 0.30;
+         } else if (leftEncoder.get() - rightEncoder.get() < -5) {
+            leftSpeed = 0.30;
+         }
+       }
+       */
+
+      //never reset navx
+      //turn to North
+      if (leftJoystick.getRawButton(9)){
+          buttonNorth = true;
+      }
+      int yawTol = 2; //yawTolerance
+      if (buttonNorth) {
+          double currentYaw = navx.getYaw();
+          leftSpeed = 0;
+          rightSpeed = 0;
+          if (currentYaw > yawTol){
+              rightSpeed = 0.20;
+              leftSpeed = -0.20;
+          } else if (currentYaw < -yawTol){
+              leftSpeed = 0.20;
+              rightSpeed = -0.20;
+          } else {
+              buttonNorth = false;
+          }
+      }
+
 
     motorLeft1.set(ControlMode.PercentOutput, leftSpeed);
     motorLeft2.set(ControlMode.PercentOutput, leftSpeed);
